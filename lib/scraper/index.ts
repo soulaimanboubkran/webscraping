b8dcb8516cf57432e11397f199d0b3749954cd1c
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-12-11 00:39:24 +0100

    fetch product from amazon

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 export async function scrapeMazonProduct(url:string){
 
     if(!url) return;
@@ -23,7 +24,54 @@ export async function scrapeMazonProduct(url:string){
 
        const $ = cheerio.load(res.data)
        const title = $('#productTitle').text().trim();
-       console.log(title)
+       const currentPrice = extractPrice(
+        $('.priceToPay span.a-price-whole'),
+        $('.a.size.base.a-color-price'),
+        $('.a-button-selected .a-color-base'),
+       
+        
+      );
+
+      const originalPrice = extractPrice(
+        $('#priceblock_ourprice'),
+        $('.a-price.a-text-price span.a-offscreen'),
+        $('#listPrice'),
+        $('#priceblock_dealprice'),
+        $('.a-size-base.a-color-price')
+      );
+      const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+      const images = 
+      $('#imgBlkFront').attr('data-a-dynamic-image') || 
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}'
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+
+
+    const currency = extractCurrency($('.a-price-symbol'))
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+    const description = extractDescription($)
+    const data = {
+        url,
+        currency: currency || '$',
+        image: imageUrls[0],
+        title,
+        currentPrice: Number(currentPrice) || Number(originalPrice),
+        originalPrice: Number(originalPrice) || Number(currentPrice),
+        priceHistory: [],
+        discountRate: Number(discountRate),
+        category: 'category',
+        reviewsCount:100,
+        stars: 4.5,
+        isOutOfStock: outOfStock,
+        description,
+        lowestPrice: Number(currentPrice) || Number(originalPrice),
+        highestPrice: Number(originalPrice) || Number(currentPrice),
+        averagePrice: Number(currentPrice) || Number(originalPrice),
+      }
+      return data;
+
     } catch (error:any) {
         console.log(error);
 
